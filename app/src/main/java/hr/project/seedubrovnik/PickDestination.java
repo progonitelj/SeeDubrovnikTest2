9508105b122c32f60df2a0ee244687cc69d4ec38
progonitelj <31577031+progonitelj@users.noreply.github.com>
2018-04-29 21:53:41 +0200

    ++ compile

--- app/src/main/java/hr/project/seedubrovnik/PickDestination.java
+++ app/src/main/java/hr/project/seedubrovnik/PickDestination.java
@@ -5,11 +5,14 @@ import android.database.SQLException;
 import android.database.sqlite.SQLiteDatabase;
 import android.database.sqlite.SQLiteOpenHelper;
 import android.net.Uri;
+import android.support.v4.widget.DrawerLayout;
+import android.support.v7.app.ActionBarDrawerToggle;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
 import android.util.Log;
+import android.view.MenuItem;
 import android.widget.Toast;
 import java.util.ArrayList;
 import java.util.List;
@@ -20,7 +23,10 @@ public class PickDestination extends AppCompatActivity {
     RecyclerView recyclerView;
     RecyclerViewListAdapter lsViewAdapter;
     List<TuristicObject> ls;
-
+    //Navbar include *start*
+    private DrawerLayout mDrawerLayout;
+    private ActionBarDrawerToggle mToggle;
+    //Navbar include *end*
 
 
     @Override
@@ -28,7 +34,13 @@ public class PickDestination extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_pick_destination);
         Log.d(TAG, "onCreate: Started!! ");
-
+        //Navbar prepare *start*
+        mDrawerLayout = (DrawerLayout) findViewById(R.id.drawer_layout);
+        mToggle = new ActionBarDrawerToggle(this, mDrawerLayout, R.string.open, R.string.close);
+        mDrawerLayout.addDrawerListener(mToggle);
+        mToggle.syncState();
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+        //Navbar prepare *end*
         try {
             SQLiteOpenHelper seeDubrovnikSQLHelper = new SeeDubrovnikDatabaseHellper(this);
             SQLiteDatabase db = seeDubrovnikSQLHelper.getReadableDatabase();
@@ -106,5 +118,17 @@ public class PickDestination extends AppCompatActivity {
         lsViewAdapter = new RecyclerViewListAdapter(this, ls);
         recyclerView.setAdapter(lsViewAdapter);
 
+
+
+
     }
+    //Navbar hamburger icon function *start*
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        if (mToggle.onOptionsItemSelected(item)) {
+            return true;
+        }
+        return super.onOptionsItemSelected(item);
+    }
+    //Navbar hamburger icon function *start*
 }
--- app/src/main/java/hr/project/seedubrovnik/WelcomeStartActivity.java
+++ app/src/main/java/hr/project/seedubrovnik/WelcomeStartActivity.java
@@ -15,31 +15,38 @@ import android.widget.Toolbar;
 //back to old
 
 public class WelcomeStartActivity extends AppCompatActivity {
-private DrawerLayout mDrawerLayout;
-private ActionBarDrawerToggle mToggle;
+    //Navbar include *start*
+    private DrawerLayout mDrawerLayout;
+    private ActionBarDrawerToggle mToggle;
+    //Navbar include *end*
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_welcome_start);
+
+        //Navbar prepare *start*
         mDrawerLayout = (DrawerLayout) findViewById(R.id.drawer_layout);
-
-        mToggle = new ActionBarDrawerToggle(this,mDrawerLayout,R.string.open,R.string.close);
+        mToggle = new ActionBarDrawerToggle(this, mDrawerLayout, R.string.open, R.string.close);
         mDrawerLayout.addDrawerListener(mToggle);
-
         mToggle.syncState();
-    getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+        //Navbar prepare *end*
+
     }
 
+    //Navbar hamburger icon function *start*
     @Override
     public boolean onOptionsItemSelected(MenuItem item) {
-        if(mToggle.onOptionsItemSelected(item)){
+        if (mToggle.onOptionsItemSelected(item)) {
             return true;
         }
         return super.onOptionsItemSelected(item);
     }
+    //Navbar hamburger icon function *start*
 
-    public void onClickStart(View view){
+
+    public void onClickStart(View view) {
         Intent intent = new Intent(this, PickDestination.class);
         startActivity(intent);
     }
