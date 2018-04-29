fa6b6ee689bdfcab0433a01f17d451d1555794f1
progonitelj <31577031+progonitelj@users.noreply.github.com>
2018-04-29 19:01:58 +0200

    ++ compile

--- app/src/main/java/hr/project/seedubrovnik/WelcomeStartActivity.java
+++ app/src/main/java/hr/project/seedubrovnik/WelcomeStartActivity.java
@@ -1,23 +1,43 @@
 package hr.project.seedubrovnik;
 
 import android.content.Intent;
+import android.support.v4.widget.DrawerLayout;
+import android.support.v7.app.ActionBarDrawerToggle;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.MenuItem;
 import android.view.View;
 import android.widget.Button;
+import android.widget.Toolbar;
 
 
 //hello world
 //back to old
 
 public class WelcomeStartActivity extends AppCompatActivity {
+private DrawerLayout mDrawerLayout;
+private ActionBarDrawerToggle mToggle;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_welcome_start);
+        mDrawerLayout = (DrawerLayout) findViewById(R.id.drawer_layout);
+
+        mToggle = new ActionBarDrawerToggle(this,mDrawerLayout,R.string.open,R.string.close);
+        mDrawerLayout.addDrawerListener(mToggle);
+
+        mToggle.syncState();
+    getSupportActionBar().setDisplayHomeAsUpEnabled(true);
     }
 
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        if(mToggle.onOptionsItemSelected(item)){
+            return true;
+        }
+        return super.onOptionsItemSelected(item);
+    }
 
     public void onClickStart(View view){
         Intent intent = new Intent(this, PickDestination.class);
